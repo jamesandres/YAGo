46e2b59873bad3ec317156841b4e8717cf0f9cd3
James Andres <jandres@conversocial.com>
2013-11-18 12:54:09 +0000

    Static assets config

--- app/settings.py
+++ app/settings.py
@@ -1,4 +1,4 @@
-# Django settings for app project.
+import os.path
 
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
@@ -69,9 +69,10 @@ STATIC_URL = '/static/'
 
 # Additional locations of static files
 STATICFILES_DIRS = (
-    # Put strings here, like "/home/html/static" or "C:/www/django/static".
-    # Always use forward slashes, even on Windows.
-    # Don't forget to use absolute paths, not relative paths.
+    os.path.join(
+        os.path.dirname(__file__),
+        'static',
+    ),
 )
 
 # List of finder classes that know how to find static files in
